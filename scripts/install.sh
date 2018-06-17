cdedff35eff6c591dd2c17d0c6cb8c6b27d53bbe
Dan Molik <dan@danmolik.com>
2018-06-17 10:08:04 -0400

    more fixups and rebuild to latest

--- scripts/install.sh
+++ scripts/install.sh
@@ -26,41 +26,40 @@ mount --rbind /dev  /mnt/gentoo/dev
 
 echo "= UPDATING MAKE.CONF ===================================================="
 
-echo <<EOF > /mnt/gentoo/etc/portage/make.conf
+cat <<<'
 CHOST="x86_64-pc-linux-gnu"
 
 CFLAGS="-march=native -O2 -pipe"
-CXXFLAGS="\${CFLAGS}"
+CXXFLAGS="${CFLAGS}"
 ACCEPT_KEYWORDS="~*"
 
 MAKEOPTS="-j4"
-# GRUB_PLATFORMS="coreboot pc"
 
 LINGUAS="en en_US"
 L10N="en en_US"
-PYTHON_TARGETS="python3_6"
-PYTHON_SINGLE_TARGET="python2_7 python3_6"
+PYTHON_TARGETS="python2_7 python3_6"
+PYTHON_SINGLE_TARGET="python3_6"
 POSTGRES_TARGETS="postgres10"
 
 CPU_FLAGS_X86="avx avx2 aes mmx sse sse2 ssse3 sse4_1 sse4_2"
 
 SYSTEM="threads jemalloc udev vim-syntax"
-SYSTEM="\${SYSTEM} jit pcre pcre-jit -bindist"
-SYSTEM="\${SYSTEM} uuid gmp audit"
+SYSTEM="${SYSTEM} jit pcre pcre-jit -bindist"
+SYSTEM="${SYSTEM} uuid gmp audit"
 AUTH=""
-NET="curl json ssh"
-NET="\${NET} -sslv3 -sslv2"
+NET="curl json"
+NET="${NET} -sslv3 -sslv2"
 
 LANGS="perl -python"
 DB="lmdb"
 VCS="git"
 
-USE="\${USE} \${SYSTEM} \${AUTH} \${LANGS} \${DB} \${VCS} \${NET}"
+USE="${USE} ${SYSTEM} ${AUTH} ${LANGS} ${DB} ${VCS} ${NET}"
 
 PORTDIR="/usr/portage"
-DISTDIR="\${PORTDIR}/distfiles"
-PKGDIR="\${PORTDIR}/packages"
-EOF
+DISTDIR="${PORTDIR}/distfiles"
+PKGDIR="${PORTDIR}/packages"
+' > /mnt/gentoo/etc/portage/make.conf
 
 echo "= BUILDING UTILITIES ===================================================="
 chroot /mnt/gentoo /bin/bash<<EOF
@@ -68,7 +67,9 @@ ln -s /etc/portage/make.conf /etc/make.conf
 source /etc/profile
 eselect profile set 22
 emerge --sync
-cat /etc/make.conf
+emerge -1 dev-util/pkgconf
+emerge --rage-clean dev-libs/glib x11-misc/shared-mime-info
 emerge -vuND --with-bdeps=y -1 @world
-emerge -1 app-portage/eix app-text/tree sys-process/lsof sys-process/htop app-editors/vim dev-vcs/git
+echo "sys-kernel/gentoo-sources symlink" >> /etc/portage/package.use/kernel
+emerge sys-apps/haveged net-misc/ntp net-misc/dhcpcd app-admin/sudo app-portage/eix app-text/tree sys-process/lsof sys-process/htop app-editors/vim dev-vcs/git sys-boot/grub app-admin/monit app-admin/rsyslog sys-kernel/gentoo-sources sys-process/vixie-cron
 EOF
